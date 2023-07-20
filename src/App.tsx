4f34637b3958ed4db2617c69f701e2e265cefe1c
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-07-20 22:59:10 +0530

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     
+      <h1 className="text-3xl bg-black text-white">Hello Ji</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
